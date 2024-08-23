69e3d7f125a00a1ff6dc4381e49c3140d6757da1
Haraldur Steinar Skúlason <hallist@hey.com>
2024-08-23 19:20:58 +0000

    fixed tabs and made main more modular

--- app/main.go
+++ app/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-contrib/gzip"
+	"github.com/russross/blackfriday/v2"
 	"go-blog/service"
 	"html/template"
 	"log"
 	"os"
 	"strconv"
 
-	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-	"github.com/russross/blackfriday/v2"
+)
+
+var (
+	posts []service.BlogPost
+	e     *gin.Engine
 )
 
 func readFile(filePath string) (string, error) {
@@ -52,8 +57,8 @@ func loadEnv() {
 }
 
 func main() {
-	// Initialize the database
-	// loadEnv()
+	//Initialize the database
+	loadEnv()
 	service.InitDatabase()
 	defer func(Db *sql.DB) {
 		err := Db.Close()
@@ -62,117 +67,113 @@ func main() {
 		}
 	}(service.Db)
 
-	e := gin.Default()
-	// use gzip for text compression
-	e.Use(gzip.Gzip(gzip.DefaultCompression))
+	initializeServer()
 
+	posts = service.GetBlogPosts()
+
+	setRoutes()
+
+	//======== Run the server
+	err := e.Run(":8080")
+	if err != nil {
+		return
+	}
+}
+
+func initializeServer() {
+	e = gin.Default()
+	e.Use(gzip.Gzip(gzip.DefaultCompression)) // use gzip for text compression
 	e.LoadHTMLGlob("templates/*")
+	e.StaticFile("/favicon.ico", "./public/static/favicon.ico")
+	e.Static("/styling", "./public/styling")
 
-	posts := service.GetBlogPosts()
+}
 
+func setRoutes() {
 	cssMain, cssPost, cssPostable, err := getCSS()
 	if err != nil {
 		log.Fatal("Error reading CSS files: ", err)
 	}
 
-	e.StaticFile("/favicon.ico", "./public/static/favicon.ico")
-	e.Static("/styling", "./public/styling")
-
-	newPost := 0
-
-	// Define the routes
-	//=========== GET / - Display the list of blog posts
-	e.GET("/", func(c *gin.Context) {
-		if newPost == 1 {
-			posts = service.GetBlogPosts()
-			newPost = 0
-		}
-		c.Header("Cache-Control", "no-cache")
-		c.HTML(200, "index.html", gin.H{
-			"posts":      posts,
-			"cssContent": cssMain,
-		})
-	})
-
-	//=========== GET /postable - can post a new blog post
-	e.GET("/postable", func(c *gin.Context) {
-		if newPost == 1 {
-			posts = service.GetBlogPosts()
-			newPost = 0
-		}
-		c.Header("Cache-Control", "no-cache")
-		c.HTML(200, "postable.html", gin.H{
-			"posts":      posts,
-			"cssContent": cssPostable,
-		})
-	})
-
-	//=========== GET images and such
+	e.GET("/", func(c *gin.Context) { showPosts(c, cssMain) })                // show all posts
+	e.GET("/postable", func(c *gin.Context) { showPostable(c, cssPostable) }) // postable site
+	e.GET("/posts/:id", func(c *gin.Context) { showPost(c, cssPost) })        // show a single post
+	e.POST("/post", func(c *gin.Context) { sendPost(c) })                     // send a post
 	e.GET("/public/*filepath", func(c *gin.Context) {
 		c.File("public/" + c.Param("filepath"))
+	}) // serve static files
+}
+
+func showPosts(c *gin.Context, cssMain template.CSS) {
+	c.Header("Cache-Control", "no-cache")
+	c.HTML(200, "index.html", gin.H{
+		"posts":      posts,
+		"cssContent": cssMain,
 	})
+}
 
-	//=========== GET /post/:id - Display a single blog post
-	e.GET("/posts/:id", func(c *gin.Context) {
-		idStr := c.Param("id")
-		id, err := strconv.Atoi(idStr)
-		if err != nil {
-			c.JSON(400, gin.H{"error": "Invalid post ID"})
-			return
-		}
-		var post *service.BlogPost
-		for _, p := range posts {
-			if p.ID == id {
-				post = &p
-				break
-			}
-		}
-
-		if post == nil {
-			c.JSON(404, gin.H{"error": "Post not found"})
-			return
-		}
-
-		safeBody := template.HTML(post.Body)
-		c.HTML(200, "post.html", gin.H{
-			"post":         post,
-			"title":        post.Title,
-			"description":  post.Description,
-			"body":         safeBody,
-			"canonicalURL": fmt.Sprintf("https://localhost:8080/posts/%d", post.ID),
-			"cssContent":   cssPost,
-		})
+func showPostable(c *gin.Context, cssPostable template.CSS) {
+	c.Header("Cache-Control", "no-cache")
+	c.HTML(200, "postable.html", gin.H{
+		"posts":      posts,
+		"cssContent": cssPostable,
 	})
+}
 
-	//========== POST /post - Create a new blog post
-	e.POST("/post", func(c *gin.Context) {
-		title := c.PostForm("title")
-		body := c.PostForm("body")
-		description := c.PostForm("description")
-		password := c.PostForm("password")
-
-		if password != os.Getenv("PASSWORD") {
-			log.Println("Password mismatch")
-			log.Println(os.Getenv("PASSWORD"))
-			c.JSON(401, gin.H{"error": "Unauthorized"})
-			return
-		}
-
-		bodyMarkdown := blackfriday.Run([]byte(body))
-
-		post := service.BlogPost{
-			Title:       title,
-			Body:        string(bodyMarkdown),
-			Description: description,
-		}
-		service.CreateBlogPost(post)
-		c.JSON(200, gin.H{"status": "posted"})
-		newPost = 1
-	})
-
-	//======== Run the server
-	err = e.Run(":8080")
+func showPost(c *gin.Context, cssPost template.CSS) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
 	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid post ID"})
 		return
 	}
+
+	//==== Find the correct post
+	var post *service.BlogPost
+	for _, p := range posts {
+		if p.ID == id {
+			post = &p
+			break
+		}
+	}
+
+	if post == nil {
+		c.JSON(404, gin.H{"error": "Post not found"})
+		return
+	}
+
+	safeBody := template.HTML(post.Body)
+	c.HTML(200, "post.html", gin.H{
+		"post":         post,
+		"title":        post.Title,
+		"description":  post.Description,
+		"body":         safeBody,
+		"canonicalURL": fmt.Sprintf("https://localhost:8080/posts/%d", post.ID),
+		"cssContent":   cssPost,
+	})
+}
+
+func sendPost(c *gin.Context) {
+	title := c.PostForm("title")
+	body := c.PostForm("body")
+	description := c.PostForm("description")
+	password := c.PostForm("password")
+
+	if password != os.Getenv("PASSWORD") {
+		log.Println("Password mismatch")
+		log.Println(os.Getenv("PASSWORD"))
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	bodyMarkdown := blackfriday.Run([]byte(body))
+
+	post := service.BlogPost{
+		Title:       title,
+		Body:        string(bodyMarkdown),
+		Description: description,
+	}
+	service.CreateBlogPost(post)
+	c.JSON(200, gin.H{"status": "posted"})
+	posts = service.GetBlogPosts()
 }
