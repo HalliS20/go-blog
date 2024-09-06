b1e2e4aeb6b59e892a691c5373c4ade4b7dede28
Haraldur Steinar Skúlason <hallist@hey.com>
2024-09-06 17:40:45 +0000

    improved cache

--- app/main.go
+++ app/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"github.com/gin-contrib/gzip"
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
 	"go-blog/config"
 	"go-blog/internal/router"
 	"log"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 var e *gin.Engine
@@ -35,4 +36,10 @@ func initializeServer() {
 	e = gin.Default()
 	e.Use(gzip.Gzip(gzip.DefaultCompression)) // use gzip for text compression
 	e.LoadHTMLGlob("templates/*")
+	e.Use(func(c *gin.Context) {
+		if c.Request.URL.Path == "/public/miniStyles/main.min.css" || c.Request.URL.Path == "/public/scripts/main.min.js" {
+			c.Header("Cache-Control", "public, max-age=31536000") // Cache for 1 year
+		}
+		c.Next()
+	})
 }
